83cb351767de67d51801bb751eb06f796251c15c
Andy <aking27@wisc.edu>
2020-02-17 11:50:27 -0600

    Switch statement created

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,10 +42,26 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
+    //@Override
     public boolean onOptionItemSelected(@NonNull MenuItem item){
         switch(item.getItemId()){
-            default:return super.onOptionsItemSelected(item);
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem1:
+                Toast.makeText(this, "Sub item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem2:
+                Toast.makeText(this, "Sub item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
         }
     }
 
